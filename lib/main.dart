723a74fd829a32f4a8e0fc9e6e27d6fcac78b0f5
Kosuke Kito <blueberryjam09@gmail.com>
2019-12-19 12:38:17 +0900

    Add logic pattern

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,8 @@ class MyApp extends StatelessWidget {
         ),
 //        child: const ViewModelPage(),
 //        child: const OptimizeViewModelPage(),
-        child: const ViewModelProviderPage(),
+//        child: const ViewModelProviderPage(),
+        child: const LogicProviderPage(),
       ),
     );
   }
@@ -101,7 +102,7 @@ class ViewModelPageContent extends StatelessWidget {
         onPressed: viewModel.increment,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -144,7 +145,8 @@ class OptimizeViewModelPage extends StatelessWidget {
         },
         builder: (context, increment) {
           print(
-              'OptimizeViewModelPage: StoreConnector builder FloatingActionButton');
+            'OptimizeViewModelPage: StoreConnector builder FloatingActionButton',
+          );
           return FloatingActionButton(
             onPressed: increment,
             tooltip: 'Increment',
@@ -213,7 +215,7 @@ class ViewModelProviderPageContent extends StatelessWidget {
         onPressed: viewModel.increment,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -252,7 +254,73 @@ class ViewModelProviderOptimizePageContent extends StatelessWidget {
         onPressed: viewModel.increment,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class Logic {
+  Logic({
+    this.increment,
+  });
+
+  final VoidCallback increment;
+}
+
+class LogicProviderPage extends StatelessWidget {
+  const LogicProviderPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('LogicProviderPage: build');
+    final store = StoreProvider.of<AppState>(context);
+    return Provider.value(
+      value: Logic(
+        increment: () {
+          store.dispatch(UpdateCount(store.state.count + 1));
+        },
+      ),
+      child: const LogicProviderPageContent(),
+    );
+  }
+}
+
+class LogicProviderPageContent extends StatelessWidget {
+  const LogicProviderPageContent();
+
+  @override
+  Widget build(BuildContext context) {
+    print('LogicProviderPageContent: build');
+    final logic = Provider.of<Logic>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Redux with ViewModel and Provider'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StoreConnector<AppState, int>(
+              converter: (store) => store.state.count,
+              builder: (context, count) {
+                print('LogicProviderPageContent: StoreConnector builder');
+                return Text(
+                  '$count',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: logic.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
